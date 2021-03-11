c447aa2125bafb33ea9d1106c6dc30b5da0fec9b
OmarAliSoliman <omarasmar9955@gmail.com>
2021-03-11 02:28:47 +0200

    first

---
+++ assets/js/main.js
@@ -0,0 +1,110 @@
+$(document).ready(function () {
+  if ($(".slider-top").length) {
+    $(".slider-top").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      dots: false,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      // rtl: currentDir == "rtl" ? true : false,
+      asNavFor: ".slider-bottom",
+    });
+    $(".slider-bottom").slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      asNavFor: ".slider-top",
+      // rtl: currentDir == "rtl" ? true : false,
+      dots: false,
+      arrows: false,
+      centerMode: false,
+      focusOnSelect: true,
+      responsive: [
+        {
+          breakpoint: 1030,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 524,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    });
+  }
+
+  if($('.companies-slider').length){
+    $(".companies-slider").slick({
+      dots: true,
+      infinite: false,
+      touchMove: true,
+      swipe: true,
+      swipeToSlide: true,
+      speed: 200,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      loop: true,
+      autoplaySpeed: 2000,
+      arrows: false,
+      infinite: true,
+      cssEase: 'linear',
+      responsive: [
+        {
+          breakpoint: 1026,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 524,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+          },
+        },
+      ],
+    });
+  }
+
+});
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+function openNav() {
+  document.getElementById("mySidenav").style.right = "0";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.right = "-200px";
+}
